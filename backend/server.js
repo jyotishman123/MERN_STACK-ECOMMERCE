85089a914b44bd959a3f72a8d7b3be5fe56023f7
Jyotishman Goswami <jyotishman2468@gmail.com>
2023-11-26 20:10:44 +0530

    Create-Route-Folder

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv'
 dotenv.config();
-import products from './data/products.js';
 import connectDB from './config/db.js';
-
+import productRoutes from './routes/productRoutes.js'
 
 const port = process.env.PORT || 5000;
 
@@ -13,16 +12,13 @@ const app = express();
 
 app.get('/',(req,res)=>{
     res.send("API is running")
-})
+});
 
-app.get('/api/products',(req,res)=>{
-    res.json(products)
-})
+app.use('/api/products', productRoutes)
 
-app.get('/api/products/:id',(req,res)=>{
-     const product = products.find((p) => p._id === req.params.id);
-     res.json(product)
-})
+ 
+
+
 
 app.listen(port,()=>{
     console.log("Server running on port", port)
