3001a1a80adad411a1a825fa2f5b54a10f84b5d0
Jyotishman Goswami <jyotishman2468@gmail.com>
2023-11-29 02:06:12 +0530

    jwt added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import Product from "../models/productModel.js";
 import User from "../models/userModel.js";
+import jwt from 'jsonwebtoken'
 
 
 
@@ -16,6 +17,20 @@ const authUser = asyncHandler(async (req,res)=>{
     const user = await User.findOne({email});
 
     if(user && (await user.matchPassword(password))){
+
+    const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET,{
+        expiresIn: '30d',
+    });
+
+    // Set JwT as HTTP-Only Cookie;
+
+    res.cookie('jwt',token,{
+        httpOnly:true,
+        secure: process.env.NODE_ENV !== 'development',
+        sameSite: 'strict',
+        maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
+    })
+
         res.json({
             _id:user._id,
             name:user.name,
